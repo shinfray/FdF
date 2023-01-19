39c945309a07fa1294832aa36b4c2addcdd19abd
shinfray <simon@hinfray.eu>
2023-01-19 11:19:41 +0100

    update

---
+++ build/srcs/main.d
@@ -0,0 +1,13 @@
+build/srcs/main.o: srcs/main.c includes/fdf.h libft/includes/libft.h \
+  libft/includes/ft_printf.h libft/includes/get_next_line_bonus.h \
+  FdF_files/minilibx_macos/mlx.h
+
+includes/fdf.h:
+
+libft/includes/libft.h:
+
+libft/includes/ft_printf.h:
+
+libft/includes/get_next_line_bonus.h:
+
+FdF_files/minilibx_macos/mlx.h:
