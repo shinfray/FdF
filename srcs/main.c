3584306e42949fb73bb0787dac30ee4f4b83dfb5
Simon Hinfray <shinfray@student.s19.be>
2023-01-31 12:27:58 +0100

    add closing window with cross button

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,26 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:44:59 by shinfray          #+#    #+#             */
-/*   Updated: 2023/01/30 16:18:40 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/01/31 12:27:06 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+typedef struct	s_data {
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_data;
 
 int	ft_check_mouse_click(int button, int x, int y, void *param)
 {
@@ -19,25 +34,31 @@ int	ft_check_mouse_click(int button, int x, int y, void *param)
 	return (0);
 }
 
-int	ft_check_key_pressed(int keycode, void *param)
+int	ft_check_key_pressed(int keycode, t_data *param)
 {
 	(void)param;
 	ft_printf("=====KEY PRESSED=====\nkeycode: %d\n", keycode);
-	if (keycode == 53)
-		exit(EXIT_SUCCESS);
+	return (0);
+}
+
+int	ft_close(t_data *param)
+{
+	mlx_destroy_window(param->mlx_ptr, param->win_ptr);
+	free(param->mlx_ptr);
+	exit(EXIT_SUCCESS);
 	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_data	window;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "FdF");
-	mlx_mouse_hook(win_ptr, &ft_check_mouse_click, NULL);
-	mlx_key_hook(win_ptr, &ft_check_key_pressed, NULL);
-	mlx_loop(mlx_ptr);
+	window.mlx_ptr = mlx_init();
+	window.win_ptr = mlx_new_window(window.mlx_ptr, 1000, 1000, "FdF");
+	mlx_mouse_hook(window.win_ptr, &ft_check_mouse_click, NULL);
+	mlx_key_hook(window.win_ptr, &ft_check_key_pressed, &window);
+	mlx_hook(window.win_ptr, ON_DESTROY, 0, &ft_close, &window);
+	mlx_loop(window.mlx_ptr);
 
 	return (0);
 }
