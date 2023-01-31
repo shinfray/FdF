6a91eff3f8d462f475cbde0d898bddbbcec7e1f5
Simon Hinfray <shinfray@student.s19.be>
2023-01-31 13:52:23 +0100

    update

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:48:01 by shinfray          #+#    #+#             */
-/*   Updated: 2023/01/30 16:06:45 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/01/31 13:10:19 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,20 @@
 # include "ft_printf.h"
 # include "get_next_line_bonus.h"
 # include "mlx.h"
+
+enum	e_events{
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+typedef struct	s_window_data {
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_window_data;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,26 +6,11 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:44:59 by shinfray          #+#    #+#             */
-/*   Updated: 2023/01/31 12:55:32 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/01/31 13:09:51 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-enum {
-	ON_KEYDOWN = 2,
-	ON_KEYUP = 3,
-	ON_MOUSEDOWN = 4,
-	ON_MOUSEUP = 5,
-	ON_MOUSEMOVE = 6,
-	ON_EXPOSE = 12,
-	ON_DESTROY = 17
-};
-
-typedef struct	s_data {
-	void	*mlx_ptr;
-	void	*win_ptr;
-}				t_data;
 
 int	ft_check_mouse_click(int button, int x, int y, void *param)
 {
@@ -51,12 +36,12 @@ int	ft_close(t_data *param)
 
 int	main(void)
 {
-	t_data	window;
+	t_window_data	window;
 
 	window.mlx_ptr = mlx_init();
 	if (window.mlx_ptr == NULL)
 		exit(EXIT_FAILURE);
-	window.win_ptr = mlx_new_window(window.mlx_ptr, 1000, 1000, "FdF");
+	window.win_ptr = mlx_new_window(window.mlx_ptr, 1920, 1080, "FdF");
 	if (window.win_ptr == NULL)
 	{
 		free(window.win_ptr);
