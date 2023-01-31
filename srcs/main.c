27cd5051de382021cf6fc499d4892cc56b634e1f
shinfray <simon@hinfray.eu>
2023-01-31 19:56:45 +0100

    update

--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,14 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:44:59 by shinfray          #+#    #+#             */
-/*   Updated: 2023/01/31 18:47:22 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/01/31 19:56:10 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-#define WINDOW_WIDTH 1920
-#define WINDOW_HEIGHT 1080
+#define WINDOW_WIDTH 1080
+#define WINDOW_HEIGHT 720
 
 void	ft_pixel_put_image(t_image_data *image, int x, int y, int color)
 {
@@ -62,9 +62,9 @@ int	ft_draw_rectangle(t_image_data *img, t_rect s_rect)
 	int	j;
 
 	i = s_rect.y;
-	j = s_rect.x;
 	while (i < s_rect.y + s_rect.height)
 	{
+		j = s_rect.x;
 		while (j < s_rect.x + s_rect.width)
 			ft_pixel_put_image(img, j++, i, s_rect.color);
 		++i;
@@ -72,21 +72,37 @@ int	ft_draw_rectangle(t_image_data *img, t_rect s_rect)
 	return (0);
 }
 
+int	ft_draw_bordure(t_image_data *img, t_rect s_rect)
+{
+	int	i;
+	int	j;
+
+
+	i = s_rect.y;
+	j = s_rect.x;
+	while (j < s_rect.x + s_rect.width)
+		ft_pixel_put_image(img, j++, i, s_rect.color);
+	++i;
+	while (i < s_rect.y + s_rect.height - 1)
+	{
+		ft_pixel_put_image(img, s_rect.x, i, s_rect.color);
+		ft_pixel_put_image(img, s_rect.x + s_rext.width - 1, i, s_rect.color);
+		++i;
+	}
+	j = s_rect.x;
+	while (j < s_rect.x + s_rect.width)
+		ft_pixel_put_image(img, j++, i, s_rect.color);
+	return (0);
+}
+
 int	main(void)
 {
 	t_data	window;
-	t_rect	s_rect;
-	//unsigned int	color;
 
-	s_rect.x = 0;
-	s_rect.y = 0;
-	s_rect.width = 100;
-	s_rect.height = 50;
-	s_rect.color = 0xFFFFFF;
 	window.mlx_ptr = mlx_init();
 	if (window.mlx_ptr == NULL)
 		exit(EXIT_FAILURE);
-	window.win_ptr = mlx_new_window(window.mlx_ptr, 1920, 1080, "FdF");
+	window.win_ptr = mlx_new_window(window.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "FdF");
 	if (window.win_ptr == NULL)
 	{
 		free(window.win_ptr);
@@ -94,7 +110,9 @@ int	main(void)
 	}
 	window.s_image.image = mlx_new_image(window.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
 	window.s_image.address = mlx_get_data_addr(window.s_image.image, &window.s_image.bpp, &window.s_image.line_length, &window.s_image.endian);
-	ft_draw_rectangle(&window.s_image, s_rect);
+	ft_draw_rectangle(&window.s_image, (t_rect){0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0x00FFFFFF});
+	ft_draw_rectangle(&window.s_image, (t_rect){0, 0, 50, 50, 0x004a78b6});
+	ft_draw_rectangle(&window.s_image, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, 0x00E398AB});
 	mlx_put_image_to_window(window.mlx_ptr, window.win_ptr, window.s_image.image, 0, 0);
 	mlx_mouse_hook(window.win_ptr, &ft_check_mouse_click, NULL);
 	mlx_key_hook(window.win_ptr, &ft_check_key_pressed, &window);
